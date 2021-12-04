0569cba6041609b549e35e1ff9a23db3eb8d2237
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-12-04 15:16:20 -0500

    🔥 Chore: Added tailwindcss, Container ui, Post component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,49 @@
 import React, { useEffect, useState } from 'react';
 
+interface Post {
+  author: {
+    id: string;
+    name: string;
+  };
+  body: string;
+  id: string;
+  publishedAt: Date;
+  title: string;
+}
+
 export function App() {
-  const [posts, setPosts] = useState<unknown>();
+  const [posts, setPosts] = useState<Post[] | null>(null);
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:4000/posts', {
-          method: 'GET',
-        });
-
-        const data = await response.json();
-        if (data) {
-          setPosts(posts);
+    fetch('http://localhost:4000/posts')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
         }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchData();
+        throw response;
+      })
+      .then((data) => {
+        setPosts(data);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {isLoading && 'Loading...'}
+
+      {error && 'Error'}
+
+      {posts?.map((post: Post) => (
+        <div>{post.body}</div>
+      ))}
+    </div>
+  );
 }
---
+++ web/src/components/Post.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface PostProps {}
+
+export function Post({}: PostProps) {
+  return <div>sadfasdfasfadsf</div>;
+}
